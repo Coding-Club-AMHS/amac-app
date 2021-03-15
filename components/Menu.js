92121d305e90945f6688490ce488c109284baad9
minamete <348563206@gapps.yrdsb.ca>
2021-03-14 21:52:20 -0400

    fixed template

--- components/Menu.js
+++ components/Menu.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import StyledMenu from '../styles/Menu.styled';
 
 const Menu = () => {
     return(
-        <section></section>
+        <StyledMenu></StyledMenu>
     )
 }
 
