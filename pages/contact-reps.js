cc542e341fe8bcc7c50e23c33b6bfe16c5fb9bf2
minamete <348563206@gapps.yrdsb.ca>
2021-04-18 14:23:58 -0400

    Removed a link for demonstration purposes

--- pages/contact-reps.js
+++ pages/contact-reps.js
@@ -6,7 +6,6 @@ export default function ContactReps() {
     <div>
       <Head>
         <title>Contact Your Reps</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       
       <Menu />
