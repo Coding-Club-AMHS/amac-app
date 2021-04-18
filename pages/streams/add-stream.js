05dc2e1e7e78443740d11b94728458aa443c5306
minamete <348563206@gapps.yrdsb.ca>
2021-04-18 14:21:04 -0400

    authentication stuff

---
+++ pages/streams/add-stream.js
@@ -0,0 +1,29 @@
+export const getServerSideProps = withSession(async function ({ req, res }) {
+    // Get the user's session based on the request
+    const user = req.session.get('user')
+  
+    if (!user) {
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false,
+        },
+      }
+    }
+  
+    return {
+      props: { user },
+    }
+  })
+  
+  const Profile = ({ user }) => {
+    // Show the user. No loading state is required
+    return (
+      <Layout>
+        <h1>Your Profile</h1>
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+      </Layout>
+    )
+  }
+  
+  export default Profile
