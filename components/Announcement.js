f0c428339a6b47e3adcd5b2c3231233cf91d2b43
Joyce Ye <46504694+yejoyce8@users.noreply.github.com>
2021-04-22 17:02:54 -0400

    hompage.module.css

--- components/Announcement.js
+++ components/Announcement.js
@@ -3,7 +3,7 @@ const Announcement = () => {
     return(
         <div>
             <StyledAnnouncement>
-                <p1 className='date'>04/22/2021</p1>
+                <p className='date'>04/22/2021</p>
                 <h1 className='heading'>Heading</h1>
                 <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus</p>
             </StyledAnnouncement>
