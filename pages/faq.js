8fb33b69de1dfa216ba6c6c2533443cb3f05e4cd
AlcxMtr <75184511+AlcxMtr@users.noreply.github.com>
2021-05-13 15:48:09 -0400

    FAQ page added

--- pages/faq.js
+++ pages/faq.js
@@ -1,19 +1,27 @@
 import Head from 'next/head'
 import Menu from '../components/Menu'
+import styles from '../styles/faq.module.css'
 
 export default function Faq() {
   return (
     <div>
+
       <Head>
         <title>FAQ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Menu />
-      <main>
-
+      <main className={styles}>
+        <h1 className={styles.h1}>Welcome To FAQ</h1>
+           <h2 className={styles.h2}>Question</h2>
+           <div className={styles.answer}>answer</div>
+           <h2 className={styles.h2}>Question</h2>
+           <div className={styles.answer}>answer</div>
+           <h2 className={styles.h2}>Question</h2>
+           <div className={styles.answer}>answer</div>
       </main>
 
     </div>
   )
-}
+  }
